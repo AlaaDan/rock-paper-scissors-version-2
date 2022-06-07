e9790e4bd66534c3b2f43d5067969160053d026e
Alaa Aldandachi <58466561+AlaaDan@users.noreply.github.com>
2022-06-07 12:49:48 +0000

    added a comment and fixed a bug

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 rock = '''
     _______
 ---'   ____)
@@ -24,19 +25,21 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
 import random
 hand_list = [rock, paper, scissors]
 game_list = [0,1,2]
 com_choice = random.choice(game_list)
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
-if user_choice >= 2 or user_choice < 0:
+#invalid input
+if user_choice >= 3 or user_choice < 0:
   print("Invalid input, you lose")
 else:
   print(f"You choose:")
   print(hand_list[user_choice])
   print("computer choose:")
   print(hand_list[com_choice])
-  #invalid input
+  
   
   #Tie 
   if user_choice ==  com_choice:
